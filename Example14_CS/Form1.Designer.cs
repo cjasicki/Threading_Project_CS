94acb1ef776ff844891de4bd196c14eb6a640d2b
cjasicki <cjasicki@gmail.com>
2020-01-19 22:36:15 -0600

    Add project files.

---
+++ Example14_CS/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Example14_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblSeed = new System.Windows.Forms.Label();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblSomething = new System.Windows.Forms.Label();
+            this.txtSleepTime = new System.Windows.Forms.TextBox();
+            this.lblErrorMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(80, 85);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(80, 125);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(547, 404);
+            this.txtResult.TabIndex = 5;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(436, 85);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop Threading";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblSeed
+            // 
+            this.lblSeed.AutoSize = true;
+            this.lblSeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeed.Location = new System.Drawing.Point(75, 50);
+            this.lblSeed.Name = "lblSeed";
+            this.lblSeed.Size = new System.Drawing.Size(94, 17);
+            this.lblSeed.TabIndex = 0;
+            this.lblSeed.Text = "Seed value:";
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(175, 50);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtSeed.Size = new System.Drawing.Size(100, 22);
+            this.txtSeed.TabIndex = 1;
+            this.txtSeed.Text = "10";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(217, 85);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Threading";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblSomething
+            // 
+            this.lblSomething.AutoSize = true;
+            this.lblSomething.Location = new System.Drawing.Point(320, 50);
+            this.lblSomething.Name = "lblSomething";
+            this.lblSomething.Size = new System.Drawing.Size(161, 17);
+            this.lblSomething.TabIndex = 6;
+            this.lblSomething.Text = "Sleep Time (in seconds)";
+            // 
+            // txtSleepTime
+            // 
+            this.txtSleepTime.Location = new System.Drawing.Point(487, 45);
+            this.txtSleepTime.Name = "txtSleepTime";
+            this.txtSleepTime.Size = new System.Drawing.Size(47, 22);
+            this.txtSleepTime.TabIndex = 7;
+            // 
+            // lblErrorMsg
+            // 
+            this.lblErrorMsg.AutoSize = true;
+            this.lblErrorMsg.Location = new System.Drawing.Point(335, 13);
+            this.lblErrorMsg.Name = "lblErrorMsg";
+            this.lblErrorMsg.Size = new System.Drawing.Size(46, 17);
+            this.lblErrorMsg.TabIndex = 8;
+            this.lblErrorMsg.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.lblErrorMsg);
+            this.Controls.Add(this.txtSleepTime);
+            this.Controls.Add(this.lblSomething);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblSeed);
+            this.Controls.Add(this.txtSeed);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnClear;
+        internal System.Windows.Forms.TextBox txtResult;
+        internal System.Windows.Forms.Button btnStop;
+        internal System.Windows.Forms.Label lblSeed;
+        internal System.Windows.Forms.TextBox txtSeed;
+        internal System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblSomething;
+        private System.Windows.Forms.TextBox txtSleepTime;
+        private System.Windows.Forms.Label lblErrorMsg;
+    }
+}
+
---
+++ Example14_CS/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Example14_CS
+{
+    public partial class Form1 : Form
+    {
+        private int iSleepCounter = 1000;
+        private int iThreadCounter = 0;
+        private Thread[] myThread = new Thread[10];
+        delegate void UpdateTextBoxDelegate(String value);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+            lblErrorMsg.Text = "";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lblErrorMsg.Text = "";
+            Int32 intSeed = 10;
+            iThreadCounter += 1;
+            if (txtSeed.Text.Trim().Length > 0)
+            {
+                intSeed = Convert.ToInt32(txtSeed.Text);
+            }
+            if (txtSleepTime.Text.Trim().Length > 0)
+            {
+                int iSleep = 1;
+                try
+                {
+                    iSleep = Convert.ToInt32(txtSleepTime.Text);
+                }
+                catch (Exception ex)
+                {
+                    lblErrorMsg.Text = "Sleep time must be an Integer value." + ex.Message;
+                }
+                if (iSleep < 10)
+                {
+                    iSleepCounter = iSleep * 1000;
+                }
+                else
+                {
+                    lblErrorMsg.Text = "Sleep time must be an Integer value less than 10.";
+                }
+
+            }
+
+            myThread[iThreadCounter] = new Thread(this.WorkerThread);
+            myThread[iThreadCounter].IsBackground = true;
+            txtResult.Text = txtResult.Text + Environment.NewLine + "Starting thread with seed: " + intSeed.ToString() + " - ThreadID: " + myThread[iThreadCounter].ManagedThreadId.ToString();
+            myThread[iThreadCounter].Start(intSeed.ToString() + " : ThreadID - " + myThread[iThreadCounter].ManagedThreadId.ToString() + ":" + txtSleepTime.Text);
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            lblErrorMsg.Text = "";
+            if (iThreadCounter > 0)
+            {
+                txtResult.Text = txtResult.Text + Environment.NewLine + "Stopping thread ID - " + myThread[iThreadCounter].ManagedThreadId.ToString();
+            }
+            else
+            {
+                txtResult.Text = txtResult.Text + Environment.NewLine + "No Thread to Stop!";
+            }
+            if (iThreadCounter > 0)
+            {
+                if (myThread[iThreadCounter] != null)
+                {
+
+                    myThread[iThreadCounter].Abort();
+                    iThreadCounter -= 1;
+                    if (iThreadCounter < 1)
+                    {
+                        txtResult.Text = txtResult.Text + Environment.NewLine + "All Threads Stopped!";
+                    }
+    
+                }
+            }
+
+        }
+
+
+        //**********************************************************************
+        //** This worker thread will display a value (the seed) in a textbox,
+        //** increment the value by 5, wait (default value 1000) milliseconds 
+        //**  as set by value in textbox and repeats
+        //**********************************************************************
+        private void WorkerThread(object objStart)
+        {
+            Boolean blnErrOccurred = false;
+            Int32 iSleepCount = 1000;//Default value 1 second.
+            Int32 intShowValue;
+            String[] strArgs = objStart.ToString().Split(':');
+            intShowValue = Convert.ToInt32(strArgs[0].Trim());
+            String strThreadID = strArgs[1];
+            Int32 intTime = Convert.ToInt32(strArgs[2].Trim());
+            while (true)
+            {
+                UpdateTextBox("Thread result: " + intShowValue.ToString() + strThreadID + " - Sleep time is " + intTime.ToString());
+                intShowValue += 5;
+
+                if (intTime > 0)
+                {
+                    try
+                    {
+                        //txtResult.Text = "This should be an error";
+                        iSleepCount = intTime * 1000; //Using the TextBox directly makes this non-thread safe.
+                    }
+                    catch (Exception ex)
+                    {
+                        lblErrorMsg.Text = "Sleep time must be an Integer value." + ex.Message;
+                        blnErrOccurred = true;
+                    }
+                }
+                //if (txtSleepTime.Text.Length > 0)
+                //{
+                //    try
+                //    {
+                //        //txtResult.Text = "This should be an error";
+                //        iSleepCount = Convert.ToInt32(txtSleepTime.Text) * 1000; //Using the TextBox directly makes this non-thread safe.
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        lblErrorMsg.Text = "Sleep time must be an Integer value." + ex.Message;
+                //        blnErrOccurred = true;
+                //    }
+                //}
+
+                if (blnErrOccurred)
+                {
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Thread.Sleep(iSleepCount);
+                }
+            }
+        }
+
+        //**********************************************************************
+        //** This routine allows cross-thread access to the textbox. The textbox
+        //** belongs to the main thread. SEE SAFE CROSS-THEADING on MSDN
+        //**********************************************************************
+        private void UpdateTextBox(String strMsg)
+        {
+            if (txtResult.InvokeRequired)
+            {
+                txtResult.Invoke(new UpdateTextBoxDelegate(UpdateTextBox), strMsg);//+ " : " + myThread.ManagedThreadId.ToString());
+            }
+            else
+            {
+                txtResult.Text = txtResult.Text + Environment.NewLine + strMsg;// + " : " + myThread.ManagedThreadId.ToString();
+            }
+        }
+
+
+    }
+}
+
